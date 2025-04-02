8fd925dc3e10362f9394284c4af4c088b80560ff
GyungA <gga612@naver.com>
2025-04-02 23:00:32 +0900

    Feat: Set up Tailwind CSS with CLI and organize project structure

--- life_guestbook/settings.py
+++ life_guestbook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps start
+    'life_guestbook',
     'guestbook_messages',
 ]
 
@@ -117,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'src/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'src',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
