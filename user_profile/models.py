e97e798ec3c6b830ceec3ad53e4460d9ca12d0ba
GyungA <gga612@naver.com>
2025-06-24 22:30:04 +0900

    Feat: Add UserProfile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user_id = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500, blank=True)
+    emoji = models.CharField(max_length=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
