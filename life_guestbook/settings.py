1eb016b4fc812e8f842dbd14f51d1f2cfcb67aa4
GyungA <gga612@naver.com>
2025-06-24 20:02:09 +0900

    Feat: Add user_profile app

--- life_guestbook/settings.py
+++ life_guestbook/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps start
-    'life_guestbook',
     'guestbook_messages',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
